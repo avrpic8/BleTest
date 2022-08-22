b2fdd5aebbc40a1ef42feb6846b22ca663fa2fd1
avrpic8 <avrpic8@gmail.com>
2022-08-23 01:03:56 +0430

    init BleManager object

--- app/src/main/java/com/smartEleectronics/bletest/MainActivity.java
+++ app/src/main/java/com/smartEleectronics/bletest/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 
 import com.clj.fastble.BleManager;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
@@ -16,13 +18,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initBle();
         iniToolbar();
     }
 
     public void initBle(){
         BleManager.getInstance().init(getApplication());
+        BleManager.getInstance().enableLog(true)
+                .setReConnectCount(1, 500)
+                .setConnectOverTime(20000)
+                .setOperateTimeout(5000);
     }
     public void iniToolbar(){
         toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.app_title);
     }
 }
